87a9c771feb9280511252b212f5e9e3105e9a5ab
Zeldarck <zeldarck.martin@gmail.com>
2020-01-31 23:35:28 +0100

    revert FOV change - inventory2 begin

---
+++ Assets/06_Scripts/__GameSpecific/Inventory2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory2 : MonoBehaviour
+{
+
+    ThrowableItem m_mainItem;
+    ThrowableItem m_secondItem;
+
+    void ExchangeItem()
+    {
+        ThrowableItem temp = m_secondItem;
+        m_secondItem = m_mainItem;
+        m_mainItem = temp;
+    }
+
+    void GiveItem(ThrowableItem a_item)
+    {
+        m_mainItem = a_item;
+    }
+}
