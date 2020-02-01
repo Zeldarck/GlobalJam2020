c56f22e8f3f449c7163c4d8d518faca219c483e1
Zeldarck <zeldarck.martin@gmail.com>
2020-02-01 20:12:27 +0100

    Material duplicate Test

---
+++ Assets/06_Scripts/__GameSpecific/TestMaterial.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMaterial : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        Material mat = new Material(renderer.material.shader);
+        renderer.material = mat;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
