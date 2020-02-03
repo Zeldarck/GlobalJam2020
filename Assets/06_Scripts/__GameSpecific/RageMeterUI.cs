53146025499247c2de43df9aeaff05f04ee086ca
Zeldarck <zeldarck.martin@gmail.com>
2020-02-03 15:08:23 +0100

    Rage Bar  fill smoothly

--- Assets/06_Scripts/__GameSpecific/RageMeterUI.cs
+++ Assets/06_Scripts/__GameSpecific/RageMeterUI.cs
@@ -12,20 +12,35 @@ public class RageMeterUI : MonoBehaviour
     [SerializeField]
     float m_intensityMultiplierShaker = 1.5f;
 
+
+    [SerializeField]
+    float m_speedFill = 1.25f;
+
+    float m_targetRageValue = 0.0f;
+
+    float m_time = 0;
     // Start is called before the first frame update
     void Start()
     {
         m_slider = GetComponent<Slider>();
         EventManager.Instance.RegisterOnRageUpdate((o, number) => OnRageUpdated(number.m_number));
+        m_slider.value = 0.0f;
+
     }
 
 
     void OnRageUpdated(float a_rageLevel)
     {
+        m_slider.value = m_targetRageValue;
         if (m_slider.value < a_rageLevel)
         {
             GetComponent<UIShaker>().Shake(m_timeMultiplierShaker * (a_rageLevel - m_slider.value), m_intensityMultiplierShaker * ( a_rageLevel - m_slider.value) );
         }
-        m_slider.value = a_rageLevel;
+        m_targetRageValue = a_rageLevel;
+    }
+
+    private void Update()
+    {
+        m_slider.value += Time.deltaTime * (m_targetRageValue - m_slider.value ) * m_speedFill;
     }
 }
