a020ee7fcec0a3fe77ca9882bc1870d167b207c7
Zeldarck <zeldarck.martin@gmail.com>
2020-02-09 22:06:00 +0100

    Fade On start

---
+++ Assets/06_Scripts/__GameSpecific/FadeUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeUI : MonoBehaviour
+{
+
+    Image m_backgroundImage;
+
+
+    [SerializeField]
+    float m_durationFading = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_backgroundImage = GetComponent<Image>();
+        EventManager.Instance.RegisterOnStart((o) => m_backgroundImage.CrossFadeAlpha(0, m_durationFading, false));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+
+}
