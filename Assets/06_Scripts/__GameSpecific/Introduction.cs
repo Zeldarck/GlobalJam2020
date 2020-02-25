b65d015ad443759e63c7c6a902ad6297a2b3ae59
Zeldarck <zeldarck.martin@gmail.com>
2020-02-25 22:55:59 +0100

    Integrate Intro - build

---
+++ Assets/06_Scripts/__GameSpecific/Introduction.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Introduction : MonoBehaviour
+{
+
+    [SerializeField]
+    AudioSource m_mainTrack;
+
+    Animator m_animator;
+
+    bool m_doOnce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!m_doOnce && Time.timeSinceLevelLoad > 2.75f )
+        {
+            m_animator.SetTrigger("Start");
+            m_mainTrack.Play();
+            m_doOnce = true;
+        }
+        if(m_doOnce && PlayerPrefs.HasKey("IntroPlayed") && Input.anyKeyDown)
+        {
+            FinishIntro();
+        }
+    }
+
+    void FinishIntro()
+    {
+        PlayerPrefs.SetInt("IntroPlayed", 1);
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
